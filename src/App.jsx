24187a90ea52fb8995a7cc0eee60db13464b786e
Antonio Manno <antonio.manno.7@gmail.com>
2024-11-07 13:01:53 +0000

    homepage first draw

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/pages/Home";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -9,7 +10,9 @@ function App() {
     <Router>
       <ScrollToTop />
       <Navbar />
-      <Routes>{/* <Route exact path="/" element={<Home/>} /> */}</Routes>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../App.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <div className="hero">
+          <div className="hero-banner">
+            <h1>FLORAL DECORATIONS & INSTALLATIONS</h1>
+            <p>Hand crafted and artisan made designs.</p>{" "}
+            <p>
+              {" "}
+              A friendly busy team with workshops in SW France and the U.K We
+              love creative ideas and grateful to be given the trust to work
+              with you.
+            </p>
+            <p>
+              {" "}
+              It’s important to know that we are committed to affordable and
+              sustainable florals, decorations and hire.
+            </p>{" "}
+            <p> -Diana & Rebecca</p>
+          </div>
+        </div>
+        <div className="links"></div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
